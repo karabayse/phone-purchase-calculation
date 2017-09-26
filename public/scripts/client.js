fae5da00fffa6c034af6887892d7f641474f64dc
Kara Bayse <karabayse@gmail.com>
2017-09-26 13:31:03 -0500

    base functionality working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,12 +6,35 @@ const accessoryPrice = 9.99;
 const spendingThreshold = 200;
 
 var bankAccountBalance = 303.91;
-var amount = 0;
+// running total of amount spent
+var amountSpent = 0;
 
-function calculatePhone(amount) {
-  return '$' + amount.toFixed(2);
+function formatAmount(amountSpent) {
+  return '$' + amountSpent.toFixed(2);
 }
 
-function calculateTax(amount) {
-  return amount * taxRate;
+function calculateTax(amountSpent) {
+  return amountSpent * taxRate;
 }
+
+// continue to purchase phones while you still have money
+while (amountSpent < bankAccountBalance) {
+  // purchase new phone
+  amountSpent = amountSpent + phonePrice;
+  // can you afford the accessory?
+  if (amountSpent < spendingThreshold) {
+    amountSpent = amountSpent + accessoryPrice;
+  }
+}
+
+// tax calculation
+amountSpent = amountSpent + calculateTax(amountSpent);
+
+console.log('Your purchase: ' + formatAmount(amountSpent));
+// your purchase: $334.76
+
+// can you afford this purchase?
+if (amountSpent > bankAccountBalance) {
+  console.log('You cannot afford this purchase. :(');
+}
+// you cannot afford this purchase
